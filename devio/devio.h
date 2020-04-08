95a14225aa461fd86c09342e5957033efe77aadd
Dimiden <dimiden@airensoft.com>
2020-04-08 13:00:29 +0900

    Current version 2.0.10 built 25 November 2018

---
+++ devio/devio.h
@@ -0,0 +1,30 @@
+#define DEVIO_VERSION "3.05"
+
+#define MULTI_CONTAINER_DELIMITER ":::"
+
+typedef safeio_ssize_t(__cdecl dllread_decl)(void *handle,
+    void *buf,
+    safeio_size_t size,
+    off_t_64 offset);
+
+typedef dllread_decl *dllread_proc;
+
+typedef safeio_ssize_t(__cdecl dllwrite_decl)(void *handle,
+    void *buf,
+    safeio_size_t size,
+    off_t_64 offset);
+
+typedef dllwrite_decl *dllwrite_proc;
+
+typedef int(__cdecl dllclose_decl)(void *handle);
+
+typedef dllclose_decl *dllclose_proc;
+
+typedef void * (__cdecl dllopen_decl)(const char *file,
+    int read_only,
+    dllread_proc *dllread,
+    dllwrite_proc *dllwrite,
+    dllclose_proc *dllclose,
+    off_t_64 *size);
+
+typedef dllopen_decl *dllopen_proc;
